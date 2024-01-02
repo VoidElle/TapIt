4a4dd157c495bba6248c73d0873052105690b942
VoidElle <lucadelc@gmail.com>
2024-01-02 22:52:33 +0100

    fix: Online - Join | PinPut

--- lib/game/online/widgets/join/new_game_online_join_lobby_pin.dart
+++ lib/game/online/widgets/join/new_game_online_join_lobby_pin.dart
@@ -2,17 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:tapit/global/utils/global_constants.dart';
 
-class NewGameOnlineJoinLobbyPin extends StatelessWidget {
+class NewGameOnlineJoinLobbyPin extends StatefulWidget {
 
   final List<TextEditingController> textEditingControllers;
 
-  NewGameOnlineJoinLobbyPin({
+  const NewGameOnlineJoinLobbyPin({
     required this.textEditingControllers,
     super.key,
   });
 
+  @override
+  State<NewGameOnlineJoinLobbyPin> createState() => _NewGameOnlineJoinLobbyPinState();
+}
+
+class _NewGameOnlineJoinLobbyPinState extends State<NewGameOnlineJoinLobbyPin> {
+
+  static const int _pinSize = 6;
+
   final List<Widget> _pinInputs = [];
-  final List<FocusNode> _focusNodes = List.generate(6, (int _) => FocusNode());
+  final List<FocusNode> _focusNodes = List.generate(_pinSize, (int _) => FocusNode());
 
   Widget _buildSinglePinInput(int index) {
     return Padding(
@@ -32,21 +40,29 @@ class NewGameOnlineJoinLobbyPin extends StatelessWidget {
           ),
         ),
         child: TextFormField(
-          controller: textEditingControllers[index],
+          controller: widget.textEditingControllers[index],
           onChanged: (String? value) {
 
             // Get the current focus
             final FocusScopeNode currentFocus = FocusScope.of(GlobalConstants.navigatorKey.currentContext!);
 
-            // If index+1 overflows the focus nodes list,
+            // If index+1 overflows the focus nodes list, or if the user removes the first inserted number
             // cancel the next focus and close the keyboard
-            if (index+1 >= _focusNodes.length) {
+            final bool unfocusOnStartPinPut = (index-1 < 0 && value == "");
+            final bool unfocusOnEndPinPut = (index+1 >= _focusNodes.length && value != "");
+            if (unfocusOnStartPinPut || unfocusOnEndPinPut) {
               currentFocus.unfocus();
               return;
             }
 
-            // Request focus of the new FormTextField
-            currentFocus.requestFocus(_focusNodes[index+1]);
+            // If the value is empty, it means that the player deleted the current number
+            // so redirect his focus to the previous focus node (index-1), if not it means the user
+            // put a number, so redirect his focus to the next focus node (index+1)
+            currentFocus.requestFocus(_focusNodes[
+                value != null && value != ""
+                    ? index + 1
+                    : index - 1
+            ]);
 
           },
           focusNode: _focusNodes[index],
@@ -70,12 +86,17 @@ class NewGameOnlineJoinLobbyPin extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
+  void initState() {
 
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < _pinSize; i++) {
       _pinInputs.add(_buildSinglePinInput(i));
     }
 
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
@@ -97,5 +118,4 @@ class NewGameOnlineJoinLobbyPin extends StatelessWidget {
           curve: Curves.easeOut,
         );
   }
-
 }
