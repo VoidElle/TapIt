5bf9e4b1d32893c284c95c2eab4aaf10e54432bd
VoidElle <lucadelc@gmail.com>
2024-03-09 12:57:21 +0100

    refactor: Added documentation to the event's socket emitter class

--- lib/game/online/utils/game_online_socket_emitter.dart
+++ lib/game/online/utils/game_online_socket_emitter.dart
@@ -1,14 +1,23 @@
 import 'package:socket_io_client/socket_io_client.dart' as socket_io;
 import 'package:tapit/game/online/enums/socket_enums.dart';
 
+/// EVERY FUNCTION / EVENT THAT IS LISTED IN THIS CLASS
+/// CAN RESULT IN A SUCCESS RESPONSE OR A FAIL RESPONSE
+/// BE CAREFUL USING THEM AND LISTEN FOR BOTH SUCCESS AND FAIL EVENT RESPONSE
 class GameOnlineSocketEmitter {
 
+  // ==============================
+  //     START LOBBY SECTION
+  // ==============================
+
+  // Function used to create a lobby
   void emitCreateLobbyEvent({
-    required socket_io.Socket socket
+    required socket_io.Socket socket,
   }) {
     socket.emit(GameOnlineSocketEvent.createLobbyRequest.text);
   }
 
+  // Function used to make a player join a lobby
   void emitJoinLobbyEvent({
     required socket_io.Socket socket,
     required String lobbyId
@@ -16,6 +25,7 @@ class GameOnlineSocketEmitter {
     socket.emit(GameOnlineSocketEvent.joinLobbyRequest.text, lobbyId);
   }
 
+  // Function used to make a player quit a lobby
   void emitQuitLobbyEvent({
     required socket_io.Socket socket,
     required String lobbyId
@@ -23,6 +33,7 @@ class GameOnlineSocketEmitter {
     socket.emit(GameOnlineSocketEvent.quitLobbyRequest.text, lobbyId);
   }
 
+  // Function used to change a player's ready status
   void emitChangeReadyStatusEvent({
     required socket_io.Socket socket,
     required String lobbyId
@@ -30,6 +41,8 @@ class GameOnlineSocketEmitter {
     socket.emit(GameOnlineSocketEvent.playerChangeReadyStatus.text, lobbyId);
   }
 
+  // Function used to start a lobby,
+  // making a lobby an actual game's match
   void emitStartLobbyEvent({
     required socket_io.Socket socket,
     required String lobbyId
@@ -37,6 +50,18 @@ class GameOnlineSocketEmitter {
     socket.emit(GameOnlineSocketEvent.startLobbyRequest.text, lobbyId);
   }
 
+  // ==============================
+  //     END LOBBY SECTION
+  // ==============================
+
+
+
+
+  // ==============================
+  //     START GAME SECTION
+  // ==============================
+
+  // Function used to score inside a game
   void emitGameScoreEvent({
     required socket_io.Socket socket,
     required String lobbyId,
@@ -44,11 +69,17 @@ class GameOnlineSocketEmitter {
     socket.emit(GameOnlineSocketEvent.gameScoreRequest.text, lobbyId);
   }
 
+  // Function used to end a game, because a
+  // player has reached the 100% of the screen
   void emitGameWinEvent({
     required socket_io.Socket socket,
     required String lobbyId,
   }) {
     socket.emit(GameOnlineSocketEvent.gameWinRequest.text, lobbyId);
   }
+
+  // ==============================
+  //     END GAME SECTION
+  // ==============================
 
 }
